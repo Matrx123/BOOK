a8b07d1650a04c550ad7ebc08a4b6a453fef9f21
vipin joshi <vpnjsh55@gmail.com>
2024-05-02 12:04:04 +0530

    iterators

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,3 +1,61 @@
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Iterators");
+
+    let v1 = vec![2, 3, 4];
+    for value in v1 {
+        println!("GOT {:?}", value);
+    }
+    //map, filter, collect()
+}
+
+#[test]
+fn calling_next_internally() {
+    let sum: u32 = Counter::new()
+        .map(|item| item * 2)
+        .sum();
+    assert_eq!(30, sum);
+}
+
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+}
+
+#[test]
+fn calling_other_def_methods() {
+    let sum: u32 = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum();
+    assert_eq!(18, sum);
 }
