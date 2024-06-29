d233c985255f1740742495484cd55bbcb4598616
vipin joshi <vpnjsh55@gmail.com>
2024-06-29 22:14:42 +0530

    refrence cycle example

--- smart_pointers/mem-leak/src/main.rs
+++ smart_pointers/mem-leak/src/main.rs
@@ -1,3 +1,47 @@
+
+ //Creating refrence cycles for understanding memory leaks
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons (_, item) => Some(item),
+            Nil => None,
+        }
+    }
+}
+
+use crate::List::{Cons, Nil};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Creating refrence cycles for understanding memory leaks");
+
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a's next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+    println!("a refrence count after creating b = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b's next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    //here is the code that will make stackoverflow error:
+    println!("a next item {:?}", a.tail())
+
 }
